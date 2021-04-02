2421f217a3d716f57d3cdb4f3f51da39d05935b5
Harry Gothold <harrygot@hotmail.com>
2021-04-02 17:20:21 +0100

    Update globals

--- shared/globals.ts
+++ shared/globals.ts
@@ -2,14 +2,8 @@ import { css } from '@emotion/react'
 
 export const globalStyles = css`
   :root {
-    --page-width: 800px;
-    --page-background: rgb(240, 240, 240);
-    --page-text: rgb(40, 40, 40);
-    --background-accent: rgb(3, 84, 133);
-    --color-text-invert: rgb(240, 240, 240);
-    --color-accent: rgb(4, 64, 100);
-    --reading-width: 60ch;
-    --white: #fff;
+    --page-background: #222;
+    --page-text: #ddd;
   }
   html,
   body {
